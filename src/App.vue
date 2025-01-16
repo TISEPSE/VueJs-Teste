29aefe3b11c744615abf7f72cebd29d3728355e9
TISEPSE <titi21player@gmail.com>
2025-01-16 12:08:27 +0100

    To do list

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,21 @@
 
   <fieldset role="group">
   <input type="text" placeholder="Tâche a effectuer" v-model="newTodo">
-  <button :disabled="newTodo.length ===0">Ajouter</button>
+  <button :disabled="newTodo.length === 0">Ajouter</button>
   </fieldset>
 
 </form>
-<div v-if="todos.length ===0">Vous n'avez pas de tache a faire :(</div>
+<div v-if="todos.length === 0">Vous n'avez pas de tache a faire :(</div>
 
 <div v-else>
   <ul>
-    <li v-for="todo in todos"
-    :key="todo.date">
-    {{ todo.title }}
+    <li v-for="todo in sortedTodo()"
+        :key="todo.date"
+        :class="{completed: todo.completed}">
+    <label>
+      <input type="checkbox" v-model="todo.completed">
+      {{ todo.title }}
+    </label>
   </li>
   </ul>
 </div>
@@ -26,7 +30,15 @@ import { ref } from 'vue';
 
 
 const newTodo = ref('')
-const todos = ref ([])
+const todos = ref ([{
+  title: 'Aller promener le chien',
+  completed: true,
+  date: Date.now(),
+}, {
+  title: "Changer l'ampoule de la voiture",
+  completed: false,
+  date: Date.now(),
+}])
 
 // Fonction pour ajouter une tâche
 const addTodo = () => {
@@ -38,7 +50,16 @@ const addTodo = () => {
   newTodo.value =""
 }
 
+const sortedTodo = () => {
+  return todos.value.toSorted((a, b) =>a.completed > b.completed ? 1 : -1)
+}
 </script>
 
 <style>
+
+.completed{
+  opacity: 50%;
+  text-decoration: line-through;
+}
+
 </style>
